a7b45eb0ae4aa50cca27be323cd9011ea26a9f6f
DigiDuncan <digiduncan@gmail.com>
2022-02-17 15:17:45 -0500

    digi also has to pee

---
+++ decbot/cogs/lol.py
@@ -0,0 +1,38 @@
+import importlib.resources as pkg_resources
+import logging
+import time
+
+import discord
+from discord.ext import commands
+
+import decbot.data
+
+logger = logging.getLogger("decbot")
+
+
+class LOLCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(
+        hidden = True
+    )
+    @commands.is_owner()
+    async def digipee(self, ctx: commands.Context):
+        """Digi also has to pee."""
+        # Gets voice channel of message author
+        voice_channel = ctx.author.voice.channel if ctx.author.voice else None
+        if voice_channel is not None:
+            vc = await voice_channel.connect()
+            with pkg_resources.path(decbot.data, "digipee.mp3") as digipee:
+                vc.play(discord.FFmpegPCMAudio(source=digipee))
+            # Sleep while audio is playing.
+            while vc.is_playing():
+                time.sleep(.1)
+            await vc.disconnect()
+        else:
+            await ctx.send(str(ctx.author.name) + "is not in a channel.")
+
+
+def setup(bot):
+    bot.add_cog(LOLCog(bot))
